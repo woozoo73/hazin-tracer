a12c612f6fce8a339c59cf3cf1af70a651485cd8
woozoo73 <woozoo73@gmail.com>
2013-06-20 16:53:18 +0900

    sample test code

--- hazin-tracer/src/main/java/com/github/woozoo73/ht/Foo.java
+++ hazin-tracer/src/main/java/com/github/woozoo73/ht/Foo.java
@@ -2,4 +2,8 @@ package com.github.woozoo73.ht;
 
 public class Foo {
 
+	public void sayHello(String name) {
+		System.out.println("Hello, " + name);
+	}
+
 }
--- hazin-tracer/src/test/java/com/github/woozoo73/ht/FooTest.java
+++ hazin-tracer/src/test/java/com/github/woozoo73/ht/FooTest.java
@@ -1,5 +1,20 @@
 package com.github.woozoo73.ht;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class FooTest {
+
+	private Foo foo;
+
+	@Before
+	public void setUp() {
+		foo = new Foo();
+	}
+
+	@Test
+	public void test() {
+		foo.sayHello("hazin");
+	}
 
 }
